f3ad0aef8a69eac1a370b4661e4898a7eb229d50
Daniel Angulo <dangulo4@gmail.com>
2020-10-15 19:54:44 -0700

    Added flash messages

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,29 +4,46 @@ exports.login = function (req, res) {
   let user = new User(req.body)
   user.login().then(function(result) {
     req.session.user = {favColor: 'blue', username: user.data.username}
-    res.send(result)
+    req.session.save(function(){
+      res.redirect('/')
+    })
   }).catch(function(err){
-    res.send(err)
+    req.flash('errors',err)
+    req.session.save(function() {
+      res.redirect('/')
+    })
   })
 };
 
-exports.logout = function () {};
+exports.logout = function (req, res) {
+  req.session.destroy(function() {
+  res.redirect('/')
+  })
+ };
 
 exports.register = function (req, res) {
   let user = new User(req.body)
-  user.register()
-  if(user.errors.length){
-res.send(user.errors)
-  } else {
-    res.send('Congrats there are no errors')
-  }
+  user.register().then(()=> {
+    req.session.user = { username: user.data.username}  
+    req.session.save(function() {
+      res.redirect('/')
+    })
+  }).catch((regErrors) => {
+    regErrors.forEach(function(error) {
+      req.flash('regErrors', error)
+      })
+      req.session.save(function(){
+        res.redirect('/')
+      })
+  })
+  
 };
 
 exports.home = function (req, res) {
   if(req.session.user){
     res.render('home-dashboard', {username: req.session.user.username})
   } else {
-    res.render('home-guest');
+    res.render('home-guest', {errors: req.flash('errors'),regErrors: req.flash('regErrors')});
   }
 };
 
