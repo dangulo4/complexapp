d0f14e3f268b8a64909f27f78e24f1ea66faf119
Daniel Angulo <dangulo4@gmail.com>
2020-10-18 18:55:30 -0700

    Created Post model and postController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,61 +1,75 @@
-const User = require('../models/User')
+const User = require('../models/User');
 
-exports.mustBeLoggedIn = function(req, res, next){
-  if(req.session.user) {
-    next()
+exports.mustBeLoggedIn = function (req, res, next) {
+  if (req.session.user) {
+    next();
   } else {
-    req.flash('errors', 'You must be logged in to perform that action')
-    req.session.save(function() {
-      res.redirect('/')
-    })
+    req.flash('errors', 'You must be logged in to perform that action');
+    req.session.save(function () {
+      res.redirect('/');
+    });
   }
-}
+};
 
 exports.login = function (req, res) {
-  let user = new User(req.body)
-  user.login().then(function(result) {
-    req.session.user = {avatar: user.avatar, username: user.data.username}
-    req.session.save(function(){
-      res.redirect('/')
+  let user = new User(req.body);
+  user
+    .login()
+    .then(function (result) {
+      req.session.user = {
+        avatar: user.avatar,
+        username: user.data.username,
+        _id: user.data._id,
+      };
+      req.session.save(function () {
+        res.redirect('/');
+      });
     })
-  }).catch(function(err){
-    req.flash('errors',err)
-    req.session.save(function() {
-      res.redirect('/')
-    })
-  })
+    .catch(function (err) {
+      req.flash('errors', err);
+      req.session.save(function () {
+        res.redirect('/');
+      });
+    });
 };
 
 exports.logout = function (req, res) {
-  req.session.destroy(function() {
-  res.redirect('/')
-  })
- };
+  req.session.destroy(function () {
+    res.redirect('/');
+  });
+};
 
 exports.register = function (req, res) {
-  let user = new User(req.body)
-  user.register().then(()=> {
-    req.session.user = { username: user.data.username, avatar: user.avatar}  
-    req.session.save(function() {
-      res.redirect('/')
+  let user = new User(req.body);
+  user
+    .register()
+    .then(() => {
+      req.session.user = {
+        username: user.data.username,
+        avatar: user.avatar,
+        _id: user.data._id,
+      };
+      req.session.save(function () {
+        res.redirect('/');
+      });
     })
-  }).catch((regErrors) => {
-    regErrors.forEach(function(error) {
-      req.flash('regErrors', error)
-      })
-      req.session.save(function(){
-        res.redirect('/')
-      })
-  })
-  
+    .catch((regErrors) => {
+      regErrors.forEach(function (error) {
+        req.flash('regErrors', error);
+      });
+      req.session.save(function () {
+        res.redirect('/');
+      });
+    });
 };
 
 exports.home = function (req, res) {
-  if(req.session.user){
-    res.render('home-dashboard')
+  if (req.session.user) {
+    res.render('home-dashboard');
   } else {
-    res.render('home-guest', {errors: req.flash('errors'),regErrors: req.flash('regErrors')});
+    res.render('home-guest', {
+      errors: req.flash('errors'),
+      regErrors: req.flash('regErrors'),
+    });
   }
 };
-
-
--- models/User.js
+++ models/User.js
@@ -1,116 +1,133 @@
-const bcrypt = require ('bcryptjs')
-const usersCollection = require('../db').db().collection('users')
-const validator = require('validator')
-const md5 = require('md5')
+const bcrypt = require('bcryptjs');
+const usersCollection = require('../db').db().collection('users');
+const validator = require('validator');
+const md5 = require('md5');
 
-let User = function(data) {
-this.data = data
-this.errors = []
-}
+let User = function (data) {
+  this.data = data;
+  this.errors = [];
+};
 
-User.prototype.cleanUp = function() {
-    if(typeof(this.data.username) !='string') {
-        this.data.username = ''
+User.prototype.cleanUp = function () {
+  if (typeof this.data.username != 'string') {
+    this.data.username = '';
+  }
+  if (typeof this.data.email != 'string') {
+    this.data.email = '';
+  }
+  if (typeof this.data.password != 'string') {
+    this.data.password = '';
+  }
+
+  // Get rid of any bogus properties
+  this.data = {
+    username: this.data.username.trim().toLowerCase(),
+    email: this.data.email.trim().toLowerCase(),
+    password: this.data.password,
+  };
+};
+
+User.prototype.validate = function () {
+  return new Promise(async (resolve, reject) => {
+    if (this.data.username == '') {
+      this.errors.push('You must provide a username');
     }
-    if(typeof(this.data.email) !='string') {
-        this.data.email = ''
+    if (
+      this.data.username != '' &&
+      !validator.isAlphanumeric(this.data.username)
+    ) {
+      this.errors.push('Username can only contain letters and numbers');
     }
-    if(typeof(this.data.password) !='string') {
-        this.data.password = ''
+    if (!validator.isEmail(this.data.email)) {
+      this.errors.push('You must provide a valid email address');
+    }
+    if (this.data.password == '') {
+      this.errors.push('You must provide a password');
+    }
+    if (this.data.password.length > 0 && this.data.password.length < 6) {
+      this.errors.push('Password must be at least 6 characters');
+    }
+    if (this.data.password.length > 50) {
+      this.errors.push('Password cannot exceed 50 characters');
+    }
+    if (this.data.username.length > 0 && this.data.password.length < 3) {
+      this.errors.push('Username must be at least 3 characters');
+    }
+    if (this.data.username.length > 30) {
+      this.errors.push('Username cannot exceed 30 characters');
     }
 
-    // Get rid of any bogus properties
-    this.data = {
-        username: this.data.username.trim().toLowerCase(),
-        email: this.data.email.trim().toLowerCase(),
-        password: this.data.password
+    // Only if username is valid then check to see if it's akready taken
+    if (
+      this.data.username.length > 2 &&
+      this.data.username.length < 31 &&
+      validator.isAlphanumeric(this.data.username)
+    ) {
+      let usernameExists = await usersCollection.findOne({
+        username: this.data.username,
+      });
+      if (usernameExists) {
+        this.errors.push('Username is already taken.');
+      }
     }
-}
-
-User.prototype.validate = function() {
-    return new Promise(async (resolve, reject) => {
-        if(this.data.username == ''){
-            this.errors.push('You must provide a username')
-        }
-        if(this.data.username != '' && !validator.isAlphanumeric(this.data.username)){
-            this.errors.push('Username can only contain letters and numbers')
-        }
-        if(!validator.isEmail(this.data.email)){
-            this.errors.push('You must provide a valid email address')
-        }
-        if(this.data.password == ''){
-            this.errors.push('You must provide a password')
-        }
-        if(this.data.password.length > 0 && this.data.password.length < 6){
-            this.errors.push('Password must be at least 6 characters')
-        }
-        if(this.data.password.length > 50){
-            this.errors.push('Password cannot exceed 50 characters')
-        }
-        if(this.data.username.length > 0 && this.data.password.length < 3){
-            this.errors.push('Username must be at least 3 characters')
-        }
-        if(this.data.username.length > 30){
-            this.errors.push('Username cannot exceed 30 characters')
-        }
-    
-        // Only if username is valid then check to see if it's akready taken
-        if(this.data.username.length > 2 && this.data.username.length < 31 && validator.isAlphanumeric(this.data.username)) {
-            let usernameExists =  await usersCollection.findOne({username: this.data.username})
-            if(usernameExists) {
-                this.errors.push('Username is already taken.')
-            }
-        }
     // Only if email is valid then check to see if it's akready taken
-        if(validator.isEmail(this.data.email)) {
-            let emailExists =  await usersCollection.findOne({email: this.data.email})
-            if(emailExists) {
-                this.errors.push('Email is already being used.')
-            }
-        }
-        resolve()
-    })
-}
+    if (validator.isEmail(this.data.email)) {
+      let emailExists = await usersCollection.findOne({
+        email: this.data.email,
+      });
+      if (emailExists) {
+        this.errors.push('Email is already being used.');
+      }
+    }
+    resolve();
+  });
+};
 
-User.prototype.login = function() {
-   return new Promise((resolve, reject) =>{
-        this.cleanUp()
-        usersCollection.findOne({username: this.data.username}).then((attemptedUser)=>{
-            if(attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
-                this.data = attemptedUser
-                this.getAvatar()
-                resolve('Congrats')
-            } else {
-                reject('Invalid username / passowrd')
-            }
-        }).catch(function() {
-            reject('Please try again later.')
-        })
-   })
-}
-
-User.prototype.register = function(){
-    return new Promise(async (resolve, reject) => {
-        // Step #1 Validate user data
-        this.cleanUp()
-        await this.validate()
-    
-        // Step #2 Only if there are no validation errors then save user data into database
-        if(!this.errors.length){
-            // hash user password
-            let salt = bcrypt.genSaltSync(10)
-            this.data.password = bcrypt.hashSync(this.data.password, salt)
-           await usersCollection.insertOne(this.data)
-           this.getAvatar()
-            resolve()
+User.prototype.login = function () {
+  return new Promise((resolve, reject) => {
+    this.cleanUp();
+    usersCollection
+      .findOne({ username: this.data.username })
+      .then((attemptedUser) => {
+        if (
+          attemptedUser &&
+          bcrypt.compareSync(this.data.password, attemptedUser.password)
+        ) {
+          this.data = attemptedUser;
+          this.getAvatar();
+          resolve('Congrats');
         } else {
-            reject(this.errors)
+          reject('Invalid username / passowrd');
         }
-    })
-}
+      })
+      .catch(function () {
+        reject('Please try again later.');
+      });
+  });
+};
 
-User.prototype.getAvatar = function() {
-    this.avatar = `https://gravatar.com/avatar/${md5(this.data.email)}?s=128`
-}
+User.prototype.register = function () {
+  return new Promise(async (resolve, reject) => {
+    // Step #1 Validate user data
+    this.cleanUp();
+    await this.validate();
 
-module.exports = User
+    // Step #2 Only if there are no validation errors then save user data into database
+    if (!this.errors.length) {
+      // hash user password
+      let salt = bcrypt.genSaltSync(10);
+      this.data.password = bcrypt.hashSync(this.data.password, salt);
+      await usersCollection.insertOne(this.data);
+      this.getAvatar();
+      resolve();
+    } else {
+      reject(this.errors);
+    }
+  });
+};
+
+User.prototype.getAvatar = function () {
+  this.avatar = `https://gravatar.com/avatar/${md5(this.data.email)}?s=128`;
+};
+
+module.exports = User;
