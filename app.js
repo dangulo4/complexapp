9daabc4990290f490f424e6ffa2b3ade407ec923
Daniel Angulo <dangulo4@gmail.com>
2020-10-21 19:32:30 -0700

    Write function to check for logged in session user to view owner posts

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
 const express = require('express');
-const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
-const flash = require('connect-flash')
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('connect-flash');
 const app = express();
 
 let sessionOptions = session({
-secret: 'Javascript is cool',
-store: new MongoStore({client: require('./db')}),
-resave: false,
-saveUninitialized: false,
-cookie: {maxAge: 1000 * 60 * 60 *24, httpOnly: true}
-})
+  secret: 'Javascript is cool',
+  store: new MongoStore({ client: require('./db') }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 1000 * 60 * 60 * 24, httpOnly: true },
+});
 
-app.use(sessionOptions)
-app.use(flash())
+app.use(sessionOptions);
+app.use(flash());
 
-app.use(function(req, res, next){
-    res.locals.user = req.session.user 
-    next()
-})
+app.use(function (req, res, next) {
+  // make current user id available on the req object
+  if (req.session.user) {
+    req.visitorId = req.session.user._id;
+  } else {
+    req.visitorId = 0;
+  }
+
+  // make user session data available from within view templates
+  res.locals.user = req.session.user;
+  next();
+});
 
 const router = require('./router');
 
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use(express.static('public'));
 app.set('views', 'views');
@@ -31,4 +39,4 @@ app.set('view engine', 'ejs');
 
 app.use('/', router);
 
-module.exports = app
+module.exports = app;
